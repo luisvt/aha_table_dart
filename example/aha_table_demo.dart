2e2c9c04dc0050ff00c0cdf542755782c2f239d1
luis <luisbigvargas@gmail.com>
2015-04-19 17:46:45 -0400

    rename some files

---
+++ example/aha_table_demo.dart
@@ -0,0 +1,21 @@
+import 'package:polymer/polymer.dart';
+
+@CustomTag('aha-table-demo')
+class AhaTableDemo extends PolymerElement{
+  AhaTableDemo.created() : super.created();
+
+  @observable ObservableList secondOptions = toObservable(['opt1', 'opt2']);
+  
+  @observable var tableData = toObservable([
+    {'first': true, 'second': 'opt1', 'third': 'one'},
+    {'first': false, 'second': 'opt2', 'third': 'two'},
+    {'first': true, 'second': 'opt1', 'third': 'three'},
+    {'first': true, 'second': 'opt1', 'third': 'four'},
+    {'first': false, 'second': 'opt2', 'third': 'five'},
+    {'first': true, 'second': 'opt1', 'third': 'six'},
+    {'first': true, 'second': 'opt1', 'third': 'seven'},
+    {'first': false, 'second': 'opt2', 'third': 'eight'},
+    {'first': true, 'second': 'opt1', 'third': 'nine'}
+  ]);
+  
+}
