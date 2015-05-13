963d12c7cf964cb8aa0cc2a9e01d0d5b1621e4cd
luis <luisbigvargas@gmail.com>
2015-05-13 00:25:39 -0400

    * enhance documentation * rename requried to required and remove duplicate

--- lib/aha_column/aha_column.dart
+++ lib/aha_column/aha_column.dart
@@ -4,27 +4,51 @@ import 'package:polymer/polymer.dart';
 class AhaColumn extends PolymerElement {
   AhaColumn.created() : super.created();
 
-  ///column name
-  @published String name = "";
-  ///column type: string, text, date, time, datetime, choice
+  /// Defines the column name.
+  @published String name = '';
+
+  /// Defines the column type. It could have next values:
+  /// 
+  /// * string
+  /// * text
+  /// * date
+  /// * time
+  /// * datetime
+  /// * choice
   @published String type = "string";
+
+  /// Sets if the column is sortable. If true, when user clicks the column
+  /// header this column is going to be sorted.
   @published bool sortable = false;
+
+  /// Sets if the column is searchable. In this case is going to appear a search
+  /// component
   @published bool searchable = false;
+
+  /// Defines if this column is editable
   @published bool editable = false;
-  @published bool requried = false;
-  //used for placeholder for empty cell.
-  @published String placeholder;
+
+  /// Defines if this column is required when editing.
+  @published bool required = false;
+
+  ///used for placeholder for empty cell.
+  @published String editorplaceholder;
+
+  /// editor default Value
   @published var defaultVal;
-  //choices read from data-choices attribute
+
+  ///choices read from data-choices attribute
   @published List choices = [];
-  //hint text in column header
+
+  /// hint text in column header
   @published String hint;
-  //placehoder for filter input
+
+  /// Search Place Holder for filter input
   @published String searchplaceholder;
 
+  /// Defines the label that is going to appears in the column header
   @published String label;
 
-  @published bool required;
-
+  /// Defines the options that the dropdown editor is going to have
   @published List<Map> options;
 }
